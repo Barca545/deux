ded6b393a213ae8af9ef3e4f5ac76c6bb9b6f1f0
Barca545 <124243414+Barca545@users.noreply.github.com>
2025-04-17 21:26:32 -0400

    - Added `FrameInputs` and `Keybinds` to resource registration

--- app/src/systems/spawn/register_resources.rs
+++ app/src/systems/spawn/register_resources.rs
@@ -1,31 +1,19 @@
-// use event::GameEventQueue;
 use game_data::{DebugElements, Selected};
-// use input::user_inputs::{FrameInputs, Keybinds, PlayerInputs};
-// use math::MouseRay;
+use inputs::{frame_inputs::FrameInputs, keybinds::Keybinds};
 use nina::world::World;
 use time::ServerTime;
 
 // Refactor:
 // -Need a settings file.
-//  Dimension settings should load in from settings file.
 //  Keybinds should load in from settings file. update default/new to reflect
 // that.
 
 // TODO: This isn't registering this is actually *adding* a resource.
 pub fn register_resources(world: &mut World,) {
-  // let grid = load_grid("5v5", "json").unwrap();
-  // let grid = Grid::new(100, 100, 1.0,).unwrap();
-
   world
     .add_resource(Selected::NONE,)
-    // .add_resource(MouseRay::default(),)
-    // .add_resource(FrameInputs::new(),)
+    .add_resource(FrameInputs::new(),)
     .add_resource(ServerTime::new(),)
     .add_resource(DebugElements::new(false, false,),)
-  // .add_resource(GameEventQueue::new(),)
-  //Add Keybinds
-  // .add_resource(Keybinds::default(),)
-  // TODO: Merge the mouse ray stuff into the player inputs
-  // .add_resource(PlayerInputs::new(),);
-  ;
+    .add_resource(Keybinds::default(),);
 }
